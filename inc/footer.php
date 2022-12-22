4a9600d6a0b7d100957b68f1b7e06deb5dc36854
FrancisDeo <103027197+FrancisDeo@users.noreply.github.com>
2022-12-23 07:02:59 +0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/FrancisDeo/PracticeSet5-06">Francis Deo Almazan</a></p>
+    </div>
+</footer>
+  </body>
+</html>
